2cb79352044a06d11f0a63e53c41c45c20ca7515
Vishwas <151127265+vishux777@users.noreply.github.com>
2024-09-24 20:43:11 +0530

    Foodify.com

---
+++ script.js
@@ -0,0 +1,15 @@
+// scroll sections
+let sections = document.querySelectorAll('section');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            sec.classList.add('show-animate');
+        }    
+    });
+}
